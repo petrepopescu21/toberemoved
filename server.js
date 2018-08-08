6a8592001437fdf105c5f57648fe10cdfb8e2971
Petre Popescu <petre.popescu@microsoft.com>
2018-08-08 10:21:22 +0300

    third

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Koa = require('koa')
 var app = new Koa()
 
 app.use(context=>{
-    context.body = "Hello World v2!"
+    context.body = "Hello World v3!"
     console.log('Sent response')
 })
 
