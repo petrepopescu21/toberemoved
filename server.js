a538c0fbcd09eb259c9ede93b8d437234fc0a47e
Petre Popescu <petre.popescu@microsoft.com>
2018-08-08 10:18:33 +0300

    Second

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var Koa = require('koa')
 var app = new Koa()
 
 app.use(context=>{
-    context.body = "Hello World!"
+    context.body = "Hello World v2!"
+    console.log('Sent response')
 })
 
 app.listen(process.env.PORT || 3000)
